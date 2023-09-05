fe9a3d29211d6db776d8ed79bc4a0f18eaba1607
admichael1 <techmikado@gmail.com>
2023-09-05 13:04:48 +0100

    1st

--- NewEmailProject/settings.py
+++ NewEmailProject/settings.py
@@ -129,6 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testmikado312@gmail.com'
-EMAIL_HOST_PASSWORD = 'djssiutpqytwnbqu'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
